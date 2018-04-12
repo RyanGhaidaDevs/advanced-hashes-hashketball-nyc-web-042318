7c0c92920440d29537524822bab79906efd7760e
Ryan Ghaida <ryaboug@gmail.com>
2018-04-12 23:13:44 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -126,13 +126,13 @@ def game_hash
 end
 
 def num_points_scored(player_name, game)
-  
+
   game.each do |team, team_hash|
-    
+
     team_hash[:players].each do |player, player_hash|
-      
+
       if player_hash[:name] == player_name
-        
+
         return player_hash[:stats][:points]
         
       end
